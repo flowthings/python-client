0dbc211df6dabeb8ece160476ec09a18d6821205
Nathan Faubion <nathan@n-son.com>
2015-01-21 10:28:16 -0600

    Release prep

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup
 
 setup(name='flowthings',
-      version='0.1.4',
+      version='1.0.0',
       description='API client for the flowthings.io platform',
-      author='flowthings.io',
+      author='flowthings',
       author_email='dev@flowthings.io',
+      url='https://github.com/flowthings/python-client',
+      download_url='https://github.com/flowthings/python-client/tarball/1.0.0',
       test_suite='tests',
       packages=['flowthings'],
       install_requires=['requests', 'websocket-client'])
