534c0cbf49f1e4d1549a4889ecd59fc0bf1058a8
Nathan Faubion <nathan@n-son.com>
2015-01-21 10:43:10 -0600

    Remove download url

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ setup(name='flowthings',
       author='flowthings',
       author_email='dev@flowthings.io',
       url='https://github.com/flowthings/python-client',
-      download_url='https://github.com/flowthings/python-client/tarball/1.0.0',
       test_suite='tests',
       packages=['flowthings'],
       install_requires=['requests', 'websocket-client'])
