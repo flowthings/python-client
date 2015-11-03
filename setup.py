d1c6189b720fe2df0fee02a8a03b4ccc7afc8d53
Nathan Faubion <nathan@n-son.com>
2015-11-03 11:58:22 -0600

    Version bump (1.0.5)

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='flowthings',
-      version='1.0.4',
+      version='1.0.5',
       description='API client for the flowthings.io platform',
       author='flowthings',
       author_email='dev@flowthings.io',
