95abf0e7988cb57c37252c1a3ea84c5022dfe20f
Tom Luczak <tom@flow.net>
2016-06-16 20:24:08 -0400

    pass verify setting through default constructor

--- flowthings/default.py
+++ flowthings/default.py
@@ -2,17 +2,14 @@ from __future__ import absolute_import
 import json
 from .utils import api_request
 
-
 __all__ = ('defaults',)
 
-
 DEFAULT = {}
-
 
 class Defaults(object):
     def __init__(self, async_lib=None, secure=True, host='api.flowthings.io',
                  version='0.1', request=api_request, encoder=json,
-                 params=DEFAULT, ws_host='ws.flowthings.io'):
+                 params=DEFAULT, ws_host='ws.flowthings.io', verify=True):
 
         if params is DEFAULT:
             params = {}
@@ -22,9 +19,14 @@ class Defaults(object):
         self.host = host
         self.ws_host = ws_host
         self.version = version
-        self.request = request
         self.encoder = encoder
         self.params = params
+
+        if not verify:
+            from functools import partial
+            self.request = partial(request, verify=False)
+        else:
+            self.request = request
 
 
 defaults = Defaults()
